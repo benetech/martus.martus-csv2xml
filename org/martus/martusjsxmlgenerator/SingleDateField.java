b9815b6c2ff2712184438ea8bf529f54141ddbdd
charles <charles@localhost>
2006-01-06 23:24:00 +0000

    starting work on dates

--- org/martus/martusjsxmlgenerator/SingleDateField.java
+++ org/martus/martusjsxmlgenerator/SingleDateField.java
@@ -5,6 +5,8 @@
  */
 package org.martus.martusjsxmlgenerator;
 
+import org.mozilla.javascript.Scriptable;
+
 public class SingleDateField extends MartusField 
 {
 	public SingleDateField()
@@ -20,6 +22,24 @@ public class SingleDateField extends MartusField
 	{
 		return DATE_TYPE;
 	}
+	
+	public String getMartusValue( Scriptable scriptable )
+	{
+		String rawDate = super.getMartusValue(scriptable);
+//		Date realDate;
+//		try 
+//		{
+//			SimpleDateFormat realDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+	//		realDate = realDateFormat.parse(rawDate);
+			return "Simple:" + rawDate;
+//		} catch (ParseException e) 
+//		{
+	//		e.printStackTrace();
+		//	return rawDate;
+		//}
+		
+	}
+
 
 	//Actual Name called by the JavaScript
 	public String getClassName() 
